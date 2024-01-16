8f9f3fb344ec95039acef6821d16eca774130b3e
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-01-16 13:56:35 -0800

    chore: wip

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,18 +1,22 @@
-import ReactDOMServer from 'react-dom/server';
-import { createInertiaApp } from '@inertiajs/react';
-import createServer from '@inertiajs/react/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import route from '../../vendor/tightenco/ziggy/dist/index.m';
-import { RouteName } from 'ziggy-js';
+import ReactDOMServer from "react-dom/server";
+import { createInertiaApp } from "@inertiajs/react";
+import createServer from "@inertiajs/react/server";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import route from "../../vendor/tightenco/ziggy/dist/index.m";
+import { RouteName } from "ziggy-js";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: ReactDOMServer.renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.tsx`,
+                import.meta.glob("./Pages/**/*.tsx"),
+            ),
         setup: ({ App, props }) => {
             global.route<RouteName> = (name, params, absolute) =>
                 route(name, params, absolute, {
@@ -24,5 +28,5 @@ createServer((page) =>
 
             return <App {...props} />;
         },
-    })
+    }),
 );
