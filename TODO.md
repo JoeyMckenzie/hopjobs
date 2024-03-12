472a8c3cc2292841046f30ecd78529e1465770f6
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-03-11 23:26:27 -0700

    chore: wip

--- TODO.md
+++ TODO.md
@@ -3,7 +3,7 @@
 ### 3/6/24 - 3/13/24
 
 - ~~Add dark mode toggle~~
-- Fix up hero, make it marketable or something
+- ~~Fix up hero, make it marketable or something~~
     - Add an email signup
 - Update login screen
 - Update registration screen
--- resources/js/Components/JobNotificationSignUp.vue
+++ resources/js/Components/JobNotificationSignUp.vue
@@ -4,13 +4,15 @@ import { Button } from '@/Components/ui/button';
 </script>
 
 <template>
-    <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="rounded-3xl px-6 sm:px-12 lg:flex lg:items-center">
+    <div class="mx-auto max-w-7xl px-6 pb-12 lg:px-8 lg:pb-16">
+        <div
+            class="rounded-3xl bg-primary dark:bg-secondary shadow-2xl px-6 py-10 sm:px-12 sm:py-16 lg:flex lg:items-center lg:p-20"
+        >
             <div class="lg:w-0 lg:flex-1">
-                <h2 class="text-3xl font-bold tracking-tight">
+                <h2 class="text-3xl font-bold tracking-tight text-white">
                     Sign up for our newsletter
                 </h2>
-                <p class="mt-4 max-w-3xl text-lg">
+                <p class="mt-4 max-w-3xl text-lg text-indigo-100">
                     Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
                     qui Lorem cupidatat commodo. Elit sunt amet fugiat.
                 </p>
@@ -23,17 +25,19 @@ import { Button } from '@/Components/ui/button';
                     <Input
                         id="email-address"
                         autocomplete="email"
+                        class="w-full bg-accent dark:bg-white"
                         name="email-address"
                         placeholder="Enter your email"
-                        required
                         type="email"
                     />
                     <span class="px-1" />
-                    <Button type="submit"> Notify me</Button>
+                    <Button type="submit" variant="secondary">
+                        Notify me
+                    </Button>
                 </form>
-                <p class="mt-3 text-sm">
+                <p class="mt-3 text-sm text-indigo-100">
                     We care about the protection of your data. Read our
-                    <a class="font-medium underline" href="#"
+                    <a class="font-medium text-white underline" href="#"
                         >Privacy Policy.</a
                     >
                 </p>
