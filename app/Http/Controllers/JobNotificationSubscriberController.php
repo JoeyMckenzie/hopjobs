8b6dadb46243350c6da6c0bf5de112d71fbdb4fa
Joey <joey.mckenzie27@gmail.com>
2024-03-27 15:50:43 -0700

    chore: wip

--- app/Http/Controllers/JobNotificationSubscriberController.php
+++ app/Http/Controllers/JobNotificationSubscriberController.php
@@ -5,16 +5,14 @@ declare(strict_types=1);
 namespace App\Http\Controllers;
 
 use App\Models\JobNotificationSubscriber;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 final class JobNotificationSubscriberController extends Controller
 {
     public function __invoke(Request $request): void
     {
-
         $request->validate([
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
+            'email' => 'required|string|lowercase|email|max:255',
         ]);
 
         JobNotificationSubscriber::create([
