17c50ea0649aeee24b22608368a7cafd4a1e0b96
Joey <joey.mckenzie27@gmail.com>
2024-04-05 10:48:37 -0700

    chore: wip

--- app/Http/Controllers/JobListingBookmarkController.php
+++ app/Http/Controllers/JobListingBookmarkController.php
@@ -12,7 +12,6 @@ final class JobListingBookmarkController extends Controller
     public function __invoke(Request $request): void
     {
         $userId = $request->user();
-        ddd($userId);
         Log::info('logged for user');
 
         session('bookmarked');
